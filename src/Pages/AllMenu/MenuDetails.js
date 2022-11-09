4170bfcae6619b68d7cee7439ec109bad2450a36
tahminarim <tahminarim9@gmail.com>
2022-11-09 15:25:26 +0100

    react photo view added

--- src/Pages/AllMenu/MenuDetails.js
+++ src/Pages/AllMenu/MenuDetails.js
@@ -1,12 +1,77 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+
 
 const MenuDetails = () => {
-    const {menu_name} = useLoaderData();
+    const { menu_name, price, img, _id, description, types } = useLoaderData();
+
 
     return (
         <div>
-            <h1> {menu_name}</h1>
+            <div className="hero  min-w-min  bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+
+                        <PhotoProvider>
+                            <PhotoView src={img}>
+                                <img src={img} alt='' className="sm:m-5 p-5 min-w lg:max-w-lg rounded-lg shadow-2xl" />
+
+                            </PhotoView>
+                        </PhotoProvider>
+                    </div>
+                    <div>
+                        <h1 className="text-5xl font-bold">{menu_name}</h1>
+                        <p className="py-6 sm:m-5 p-5">{description}</p>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='grid lg:grid-cols-3'>
+                <div className='grid justify-center items-center'>
+                    <p className='text-sm lg:text-xl text-green-600 font-bold text-center'>{types[0].one} {menu_name}</p>
+                    <p className='text-green-600  text-center'> {price}€</p>
+                    <div>
+                        <PhotoProvider>
+                            <PhotoView src={types[0].img}>
+                                <img src={types[0].img} alt='' className="sm:m-5 p-5 min-w lg:max-w-xs rounded-lg  shadow-5xl" />
+                            </PhotoView>
+                        </PhotoProvider>
+                    </div>
+
+                </div>
+
+                <div className='grid justify-center items-center'>
+                    <p className='text-sm lg:text-xl text-green-600 font-bold text-center'>{types[1].two} {menu_name}</p>
+                    <p className='text-green-600  text-center'> {price}€</p>
+                    <div>
+                        <PhotoProvider>
+                            <PhotoView src={types[1].img}>
+                                <img src={types[1].img} alt='' className="sm:m-5 p-5 min-w lg:max-w-xs rounded-lg  shadow-5xl" />
+                            </PhotoView>
+                        </PhotoProvider>
+                    </div>
+                </div>
+
+                <div className='grid justify-center items-center'>
+                    <p className='text-sm lg:text-xl text-green-600 font-bold text-center'>{types[2].three} {menu_name}</p>
+                    <p className='text-green-600  text-center'> {price}€</p>
+
+                    <div>
+                        <PhotoProvider>
+                            <PhotoView src={types[2].img}>
+                                <img src={types[2].img} alt='' className="sm:m-5 p-5 min-w lg:max-w-xs rounded-lg  shadow-5xl" />
+                            </PhotoView>
+                        </PhotoProvider>
+                    </div>
+                </div>
+            </div>
+            <div className='grid justify-center'>
+                <Link to={`/checkout/${_id}`}> <button className="btn btn-success text-white m-2 text-xl lg:text-3xl mt-2">Order Now</button></Link>
+            </div>
         </div>
     );
 };
--- src/Pages/AllMenu/SingleMenu.js
+++ src/Pages/AllMenu/SingleMenu.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const SingleMenu = ({menu}) => {
-    const {_id, img,description, price, menu_name} = menu;
-    return (
-<div className="hero  bg-base-200">
-  <div className="hero-content flex-col lg:flex-col">
-    <img alt="dish" src={img} className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">{menu_name}</h1>
-      <p className="py-6">{price}€ per person</p>
 
-       {
-       description.length>150 ?
-      <p> {description.slice(0,100) + '...'}
-          <Link to={`/allmenu/${_id}`}> <button className="btn  btn-outline btn-success">See More </button></Link>
-      </p>
-      :
-      <p>{description}</p>
-      }
-      
-  
-      <Link to={`/checkout/${_id}`}> <button className="btn btn-success">Buy Now</button></Link>
+const SingleMenu = ({ menu }) => {
+  const { _id, img, description, price, menu_name } = menu;
+  return (
+    <div className="hero  bg-base-200">
+      <div className="hero-content flex-col lg:flex-col">
 
+        <div>
+          <PhotoProvider>
+            <PhotoView src={img}>
+              <img alt="dish" src={img} className="max-w-sm rounded-lg shadow-2xl" />
+
+
+            </PhotoView>
+          </PhotoProvider>
+        </div>
+        <div className='grid'>
+          <h1 className="text-5xl font-bold">{menu_name}</h1>
+          <p className="py-6">{price}€ per person</p>
+
+          {
+            description.length > 150 ?
+              <p> {description.slice(0, 100) + '...'}
+                <Link to={`/allmenu/${_id}`}> <button className="btn  btn-outline btn-success mt-2">See More </button></Link>
+              </p>
+              :
+              <p>{description}</p>
+          }
+
+
+          <Link to={`/checkout/${_id}`}> <button className="btn btn-success mt-2">Buy Now</button></Link>
+
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default SingleMenu;
